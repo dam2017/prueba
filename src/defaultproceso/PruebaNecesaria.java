d82816c3ad46a8ebc4aba598f3fb5dda1c426714
IrvinLaD <IrvinLaD@IRVINLAD-PC>
2017-03-28 18:15:19 +0200

    cambio de ivan

--- src/defaultproceso/PruebaNecesaria.java
+++ src/defaultproceso/PruebaNecesaria.java
@@ -5,6 +5,9 @@ public class PruebaNecesaria {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Asi son las cosas! y asi se las hemos contado");
+
+//Edicion guillermo
+System.out.println("guille es tonto");
 	}
 
 }
