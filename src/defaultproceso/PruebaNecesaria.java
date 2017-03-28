d363f7b36e699c4b4ed182df84b049136bf9ae44
IrvinLaD <IrvinLaD@IRVINLAD-PC>
2017-03-28 17:20:20 +0200

    Version nueva con mensajes ampliado

--- src/defaultproceso/PruebaNecesaria.java
+++ src/defaultproceso/PruebaNecesaria.java
@@ -4,7 +4,7 @@ public class PruebaNecesaria {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Asi son las cosas! y asi se las hemos contado");
 	}
 
 }
